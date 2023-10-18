da1204fb743459d8c0d2eb21b9463613316b76dc
AtleSkaanes <atle.skaanes@gmail.com>
2023-10-18 22:38:01 +0200

    Changed to physics based

--- Assets/Player/Scripts/PlayerMovementScript.cs
+++ Assets/Player/Scripts/PlayerMovementScript.cs
@@ -4,37 +4,52 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.UI.GridLayoutGroup;
 
+[RequireComponent(typeof(Rigidbody))]
+[DisallowMultipleComponent]
 public class PlayerMovementScript : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private InputActionReference move;
     [SerializeField] private float mouseSensitivity = 1f; 
     private float turner;
     private float looker;
 
-    [SerializeField] private Camera camera;
+    [SerializeField] private Camera playerCamera;
+    private Rigidbody rb;
 
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        playerCamera = Camera.main;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         turner = Input.GetAxis("Mouse X") * mouseSensitivity;
         looker = -Input.GetAxis("Mouse Y") * mouseSensitivity;
         if (turner != 0)
         {
-            //Code for action on mouse moving right
             transform.eulerAngles += new Vector3(0, turner, 0);
         }
         if (looker != 0)
         {
-            //Code for action on mouse moving right
-            camera.transform.eulerAngles += new Vector3(looker, 0, 0);
+            playerCamera.transform.eulerAngles += new Vector3(looker, 0, 0); 
+            Vector3 localEulerAngles = playerCamera.transform.eulerAngles;
+            localEulerAngles.x = ModularClamp(localEulerAngles.x, -70, 100);
+            playerCamera.transform.eulerAngles = localEulerAngles;
         }
 
         Vector2 moveVec = move.action.ReadValue<Vector2>();
-        transform.Translate(new Vector3(moveVec.x, 0, moveVec.y) * speed * Time.deltaTime);
+        rb.AddRelativeForce(new Vector3(moveVec.x, 0, moveVec.y) * movementSpeed * Time.deltaTime);
     }
+
+    private float ModularClamp(float val, float min, float max, float rangemin = -180f, float rangemax = 180f)
+    {
+        var modulus = Mathf.Abs(rangemax - rangemin);
+        if ((val %= modulus) < 0f) val += modulus;
+        return Mathf.Clamp(val + Mathf.Min(rangemin, rangemax), min, max);
+    }
+
 }
+
